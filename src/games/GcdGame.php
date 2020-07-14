89cfbad483bc020c778ac72d369cdf0101956c74
nikitovskij <nikitovskij@example.com>
2020-07-14 21:07:52 +0200

    adding GcdGame

--- src/games/GcdGame.php
+++ src/games/GcdGame.php
@@ -6,21 +6,11 @@ function generateGcdGameQuestion($start = 0, $end = 100)
 {
     $firstNum = rand($start, $end);
     $secondNum = rand($start, $end);
+    
+    $question = "{$firstNum} {$secondNum}";
+    $correctAnswer = findGcd($firstNum, $secondNum);
 
-    return "{$firstNum} {$secondNum}";
-}
-
-function checkGcdGameAnswer($numbers, $playerAnswer)
-{
-    [$firstNum, $secondNum] = explode(' ', $numbers);
-    $result = findGcd($firstNum, $secondNum);
-    return $result == $playerAnswer;
-}
-
-function getCorrectGcdGameAnswer($gameQuestionNum)
-{
-    [$firstNum, $secondNum] = explode(' ', $gameQuestionNum);
-    return findGcd($firstNum, $secondNum);
+    return [$question, $correctAnswer];
 }
 
 function findGcd($firstNum, $secondNum)
