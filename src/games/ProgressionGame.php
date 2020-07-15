4d263969a91fc3eec3e953dc76e55a8523bbf47a
nikitovskij <nikitovskij@example.com>
2020-07-15 21:41:42 +0200

    refactoring ProgressionGame

--- src/games/ProgressionGame.php
+++ src/games/ProgressionGame.php
@@ -2,17 +2,31 @@
 
 namespace BrainGames\Games\ProgressionGame;
 
+use function BrainGames\GameCore\run as start;
+
 const NUMS_IN_PROGRESSION = 9;
 
-function generateProgressionGameQuestion($start = 0, $end = 100)
+function run()
 {
-    $progression = getProgression(rand($start, $end), NUMS_IN_PROGRESSION);
+    return start(gameRule(), function () {
+        return generateProgressionGameQuestion();
+    });
+}
+
+function gameRule()
+{
+    return 'What number is missing in the progression?' . PHP_EOL;
+}
+
+function generateProgressionGameQuestion()
+{
+    $progression = getProgression(rand(0, 100), NUMS_IN_PROGRESSION);
     $guessingNum = rand(0, 9);
     $correctAnswer = $progression[$guessingNum];
     $progression[$guessingNum] = '..';
     $progressionQuestion = implode(' ', $progression);
 
-    return [$progressionQuestion, $correctAnswer];
+    return [$progressionQuestion, (string) $correctAnswer];
 }
 
 function getProgression($initNum, $numsInProgression)
